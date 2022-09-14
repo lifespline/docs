a2c440e1a8fb8911210f8c6287a0ae7955c228d1
diogo <dipm@novonordisk.com>
2022-09-14 15:44:39 +0000

    dev: change docs theme to furo, add sphinx extensions

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,16 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    # display docs build time
+    'sphinx.ext.duration',
+
+    # process docstrings in src
+    'sphinx.ext.autodoc',
+
+    # test code snippets
+    'sphinx.ext.doctest',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -24,5 +33,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 html_static_path = ['_static']
